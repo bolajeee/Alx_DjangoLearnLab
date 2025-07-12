181aeec94483945bcbc46c9155f527a592358316
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-07-12 20:47:00 +0100

    Create admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+frmo .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ['title', 'author']
+    list_filter = ['publication_year']
+
+admin.site.register(Book, BookAdmin)
