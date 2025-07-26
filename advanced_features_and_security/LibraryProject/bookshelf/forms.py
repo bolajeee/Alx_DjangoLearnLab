d01aa3741754ddf6c28edc0c754584bd2026603f
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-07-26 22:47:10 +0100

    Example form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
 class BookSearchForm(forms.Form):
     title = forms.CharField(required=False, max_length=100)
