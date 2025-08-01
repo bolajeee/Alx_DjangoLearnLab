99aa4b4c196bc9d079a23b3354ffff185bc63827
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-08-01 23:01:41 +0100

    admin

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -12,4 +13,7 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
