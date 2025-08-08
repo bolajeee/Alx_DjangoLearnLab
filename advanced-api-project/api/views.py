02fe56b7afeb547b0ddf4eccdcc47bac40d3f18f
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-08-08 22:26:58 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import rest_framework 
 
 # ------------------------
 # LIST VIEW
