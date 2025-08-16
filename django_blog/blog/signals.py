36be0317157fe3d903d888a8ae0d33e35b87195c
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-08-16 15:02:59 +0100

    Update to signals and alx checker debug

--- django_blog/blog/signals.py
+++ django_blog/blog/signals.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        # Create a new Profile only when a User is first created
+        Profile.objects.get_or_create(user=instance)
     else:
+        # Update the existing Profile when the User is updated
         instance.profile.save()
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     # Comments url
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment_create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_update'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete')
 
 ]
