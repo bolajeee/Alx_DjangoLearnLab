322e8010b55f59ff54803bda1fdc9c8755a3d4f3
bolajeee <ibrahimomobolaji1999@gmail.com>
2025-08-23 00:19:42 +0100

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -9,4 +9,8 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
    path('', include(router.urls)),
+
+   # Other URL patterns...
+   path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
+   path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
 ]
